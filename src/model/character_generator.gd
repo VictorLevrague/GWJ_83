9d260120a18c8ab51393e57cd333b55aed9dab38
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-17 15:57:29 +0200

    TEst character art

--- src/model/character_generator.gd
+++ src/model/character_generator.gd
@@ -10,7 +10,8 @@ func create_character():
     var nb_negative_actions: int = randi_range(0, RANGE_NB_NEGATIVE_ACTIONS) if nb_positive_actions else randi_range(1, RANGE_NB_NEGATIVE_ACTIONS)
     var character = Character.new()
     character.label_name = ""
-    character.illustration = load("res://icon.svg")
+    #character.illustration = load("res://icon.svg")
+    character.illustration = load("res://assets/character_illustrations/character_1.png")
     character.add_actions("Positive", nb_positive_actions)
     character.add_actions("Negative", nb_negative_actions)
     return character
