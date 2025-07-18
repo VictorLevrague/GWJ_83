bb26099d479f49a79da3c3d3493737a9146a66e9
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-18 17:20:17 +0200

    Rules button

--- src/ui/main_screen/rules_layer.gd
+++ src/ui/main_screen/rules_layer.gd
@@ -2,3 +2,10 @@ extends CanvasLayer
 
 func _on_rules_button_pressed() -> void:
     hide()
+
+func _input(event: InputEvent) -> void:
+    if Input.is_action_pressed("ui_cancel"):
+        show()
+
+func _on_rules_opener_pressed() -> void:
+    visible = not visible
