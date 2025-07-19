a471b7f07f24d1445352f5400575a0d14391ffd0
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-19 22:24:41 +0200

    Changed mouse parameter

--- src/autoload/mouse_controller.gd
+++ src/autoload/mouse_controller.gd
@@ -2,6 +2,6 @@ extends Node
 
 func _process(_delta):
     if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        Input.set_custom_mouse_cursor(load("res://assets/icons/hammer_pressed.png"), Input.CURSOR_IBEAM)
+        Input.set_custom_mouse_cursor(load("res://assets/icons/hammer_pressed.png"), Input.CURSOR_ARROW)
     else:
         Input.set_custom_mouse_cursor(load("res://assets/icons/hammer.png"), Input.CURSOR_ARROW)
