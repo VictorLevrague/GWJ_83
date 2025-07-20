7ccbd3cfba5b2d0919bdb503f651808887af9e59
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-20 18:34:20 +0200

    New mouse icon without the left click method
    There was a bug when web export

--- src/autoload/mouse_controller.gd
+++ src/autoload/mouse_controller.gd
@@ -1,7 +1,7 @@
 extends Node
 
-func _process(_delta):
-    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        Input.set_custom_mouse_cursor(load("res://assets/icons/hammer_pressed.png"), Input.CURSOR_ARROW)
-    else:
-        Input.set_custom_mouse_cursor(load("res://assets/icons/hammer.png"), Input.CURSOR_ARROW)
+#func _process(_delta):
+    #if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+        #Input.set_custom_mouse_cursor(load("uid://dqah6bcfp8ry0"), Input.CURSOR_ARROW)
+    #else:
+        #Input.set_custom_mouse_cursor(load("uid://cxcwfct2tyn35"), Input.CURSOR_ARROW)
--- src/ui/main_screen/defeat_layer.gd
+++ src/ui/main_screen/defeat_layer.gd
@@ -1,14 +1,22 @@
 extends CanvasLayer
 
+var defeat_by_cover_loss: bool = false
+var defeat_by_no_people_left_to_judge: bool = false
+
 func _on_hide_defeat_button_pressed() -> void:
     %DefeatPanel.visible = not %DefeatPanel.visible
-    %DevilAngry.visible = not %DevilAngry.visible
+    if defeat_by_no_people_left_to_judge:
+        %DevilAngry.visible = not %DevilAngry.visible
 
 func update_text(type: String):
     match type:
         "No_people_left_to_judge":
+            defeat_by_no_people_left_to_judge = true
+            defeat_by_cover_loss = false
             %DefeatLabel.text = "THAT IS NOT ENOUGH SOULS !!!\n\nGO BACK TO HELL"
             %DevilAngry.show()
         "Cover_loss":
+            defeat_by_no_people_left_to_judge = false
+            defeat_by_cover_loss = true
             %DefeatLabel.text = "WAIT A MINUTE !!! You're not the purgatory judge, you filthy demon.\n\nGET OUT"
             %DevilAngry.hide()
